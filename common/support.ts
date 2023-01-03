19a318fb2b369f7a6000aea27e879c8c1c3355e1
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MBP.fios-router.home>
2023-01-03 14:43:53 -0500

    Fix support file

--- common/support.ts
+++ common/support.ts
@@ -1,21 +1,22 @@
-// export const send = ({ name, email, twitter, linkedin, message, webhook }) => {
-//   let twitterURL = '';
-//   let linkedinURL = '';
+export const send = ({ name, email, twitter, linkedin, message, webhook }) => {
+  let twitterURL = '';
+  let linkedinURL = '';
 
-//   if (twitter) {
-//     const twitterProfileURL = `https://twitter.com/${twitter.replace('@', '')}`;
-//     twitterURL = `<${twitterProfileURL}|${twitter}>`;
-//   }
-//   if (linkedin) {
-//     const linkedinProfileURL = `https://linkedin.com/${linkedin.replace('@', '')}`;
-//     linkedinURL = `<${linkedinProfileURL}|${linkedin}>`;
-//   }
-//   try {
-//     webhook.send({
-//       text: `\n*Name:* ${name}\n*Email:* ${email}\n*Twitter:* ${twitterURL}\n*LinkedIn:* ${linkedinURL}\n*Message:* ${message}`,
-//     });
-//     return true;
-//   } catch (e) {
-//     return false;
-//   }
-// };
+  if (twitter) {
+    const twitterProfileURL = `https://twitter.com/${twitter.replace('@', '')}`;
+    twitterURL = `<${twitterProfileURL}|${twitter}>`;
+  }
+  if (linkedin) {
+    const linkedinProfileURL = `https://linkedin.com/${linkedin.replace('@', '')}`;
+    linkedinURL = `<${linkedinProfileURL}|${linkedin}>`;
+  }
+  try {
+    console.log('response');
+    // webhook.send({
+    //   text: `\n*Name:* ${name}\n*Email:* ${email}\n*Twitter:* ${twitterURL}\n*LinkedIn:* ${linkedinURL}\n*Message:* ${message}`,
+    // });
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
